2c0936cbe77f541268f2f95e0cf517ef18b4d58e
afas2004 <afahmi2004@gmail.com>
2025-01-27 16:28:30 +0800

    final touch up

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BeeHive App',
+      title: 'BookHive App',
       theme: ThemeData(primarySwatch: Colors.yellow),
       initialRoute: '/',
       routes: {
